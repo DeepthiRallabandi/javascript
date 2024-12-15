f3fa37945ebcc03ed9eb1f7e687009db5dbc97c0
DeepthiRallabandi <deepthirallabandi3@gmail.com>
2024-12-15 13:14:54 +0530

    javascript completed

---
+++ arrowfuction.js
@@ -0,0 +1,7 @@
+function deepthiInfo()//function definition
+{
+    console.log("this is Simple function");
+    console.log("This is another statemnt")
+}
+
+deepthiInfo();//function calling
---
+++ string.js
@@ -0,0 +1,6 @@
+let studentname="rallabandideepthi"
+console.log(studentname);
+console.log(studentname.startsWith('rallabandi'));
+console.log(studentname.endswith('deepthi'));
+console.log(studentname.repeat(2));
+console.log(studentname.includes('deepthi'));
